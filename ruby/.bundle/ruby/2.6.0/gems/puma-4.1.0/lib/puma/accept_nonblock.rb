d1583579b275dc5ebd2e62ddbdf0cf066f4d042c
sone <alftaketomo@gmail.com>
2021-07-26 07:18:45 +0000

    first commit

---
+++ ruby/.bundle/ruby/2.6.0/gems/puma-4.1.0/lib/puma/accept_nonblock.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'openssl'
+
+module OpenSSL
+  module SSL
+    class SSLServer
+      unless public_method_defined? :accept_nonblock
+        def accept_nonblock
+          sock = @svr.accept_nonblock
+
+          begin
+            ssl = OpenSSL::SSL::SSLSocket.new(sock, @ctx)
+            ssl.sync_close = true
+            ssl.accept if @start_immediately
+            ssl
+          rescue SSLError => ex
+            sock.close
+            raise ex
+          end
+        end
+      end
+    end
+  end
+end
