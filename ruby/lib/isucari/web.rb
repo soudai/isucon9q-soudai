ea960e1ffbfa107ee322e4f41e202e3f02d1279d
soudai <sone@have-fun.tech>
2021-07-28 13:59:27 +0900

    queryエラーの修正

--- ruby/lib/isucari/web.rb
+++ ruby/lib/isucari/web.rb
@@ -385,7 +385,7 @@ module Isucari
             "    INNER JOIN `users` ON `users`.`id` = `items`.`seller_id`" \
             "    LEFT JOIN `users` AS `buyer_stats` ON `buyer_stats`.`id` = `items`.`buyer_id` " \
             "WHERE `items`.`buyer_id` = ? AND `items`.`status` IN (?, ?, ?, ?, ?)" \
-            "ORDER BY `items`.`created_at` DESC, `items`.`id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}",
+            "ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}",
             user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP,
             user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP
           )
